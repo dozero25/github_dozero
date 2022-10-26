c0272b5029648f58a0c2020cace2768ddfe42104
dozero25 <ehdud05264@naver.com>
2022-10-27 00:44:55 +0900

    주사위 번호 룰렛 숫자를 입력받아 같은 숫자 3개 시, 같은 숫자 2개 시, 같은 숫자가 없을 시 차등으로 상금 지급 숫자를 입력 받는 것이 마음에 안듬  random을 통해 룰렛을 만들 예정

--- Java Intellij 실습/JavaStudy_20221018/src/BackJun/DiceThree.java
+++ Java Intellij 실습/JavaStudy_20221018/src/BackJun/DiceThree.java
@@ -9,15 +9,40 @@ public class DiceThree {
         int num1 = 0;
         int num2 = 0;
         int num3 = 0;
+        int reward = 0;
 
+        System.out.print("숫자 세개를 입력하세요 : ");
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+        num3 = scanner.nextInt();
 
-
-
-
-
-
-
-
+        if(num1 == num2 ){
+            if(num1 == num3){
+                reward = 10000+num1*1000;
+                System.out.println(num1+", "+num2+", "+num3+" 보상 : "+reward+"원");
+            }else if(num1 != num3){
+                reward = 1000+num1*100;
+                System.out.println(num1+", "+num2+", "+num3+" 보상 : "+reward+"원");
+            }
+        }
+        else {
+            if (num1 == num3) {
+                reward = 1000 + num1 * 100;
+                System.out.println(num1 + ", " + num2 + ", " + num3 + " 보상 : " + reward + "원");
+            }
+            else {
+                if(num1 > num2 && num1 > num3){
+                    reward = num1 * 100;
+                    System.out.println(num1 + ", " + num2 + ", " + num3 + " 보상 : " + reward + "원");
+                } else if (num2 > num3 && num2 > num1) {
+                    reward = num1 * 100;
+                    System.out.println(num1 + ", " + num2 + ", " + num3 + " 보상 : " + reward + "원");
+                } else if (num3 > num1 && num3 > num2){
+                    reward = num1 * 100;
+                    System.out.println(num1 + ", " + num2 + ", " + num3 + " 보상 : " + reward + "원");
+                }
+            }
+        }
 
     }
 }
