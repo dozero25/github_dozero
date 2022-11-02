cc36b2106b12234cebaa20ab5ee0fc50b4688e19
dozero25 <ehdud05264@naver.com>
2022-11-02 19:27:58 +0900

    11/2 CentralControl 배열 지정해서 출력하기

--- 코리아 IT 학원/src/main/java/J14_참조자료형캐스팅/CentralControl.java
+++ 코리아 IT 학원/src/main/java/J14_참조자료형캐스팅/CentralControl.java
@@ -1,27 +1,40 @@
 package J14_참조자료형캐스팅;
 
-// 중앙제어
-public class CentralControl {
-    // power를 implements한 객체면 어느 객체가 와도 사용 가능하다.
-    private Power device1;
-    private Power device2;
-    private Power device3;
+public class CentralControl { // 배열처리
+    private Power[] deviceArray;
 
-    public CentralControl(Power device1, Power device2, Power device3) {
-        this.device1 = device1;
-        this.device2 = device2;
-        this.device3 = device3;
+    public CentralControl(Power[] deviceArray) {
+        this.deviceArray = deviceArray;
     }
-
+    public void addDevice(Power device){
+        int emptyIndex = checkEmpty();
+        if(emptyIndex == -1){
+            System.out.println("더 이상 장치를 연결할 수 없습니다.");
+            System.out.println();
+            return;
+        }
+        deviceArray[emptyIndex] = device; // checkempty를 받은 emptyIndex에 device를 넣는다.
+        System.out.println(device.getClass().getSimpleName() + "장치가 연결되었습니다.");
+        System.out.println();
+    }
+    private int checkEmpty(){
+        for(int i = 0; i < deviceArray.length; i++){
+            if(deviceArray[i] == null){ // 빈공간 찾기
+                return i;
+            }
+        }
+        return -1; // 공간이 없을 경우 -1로 리턴
+    }
     public void powerOn(){
-        device1.on();
-        device2.on();
-        device3.on();
+        for(int i=0;i<deviceArray.length;i++){
+            if(deviceArray[i] == null)continue; // 하나의 명령이면 중괄호 생략 가능
+            deviceArray[i].on();
+        }
     }
-
     public void powerOff(){
-        device1.off();
-        device2.off();
-        device3.off();
+        for(int i=0;i<deviceArray.length;i++){
+            if(deviceArray[i] == null)continue;// 하나의 명령이면 중괄호 생략 가능
+            deviceArray[i].off();
+        }
     }
 }
--- 코리아 IT 학원/src/main/java/J14_참조자료형캐스팅/Main.java
+++ 코리아 IT 학원/src/main/java/J14_참조자료형캐스팅/Main.java
@@ -10,18 +10,23 @@ public class Main {
         LED led = new LED();
         Speaker speaker = new Speaker();
 
-        CentralControl centralControl1 = new CentralControl(computer1, computer2, computer3);
+        CentralControl centralControl1 = new CentralControl(new Power[5]);
         // 묵시적으로 power가 생략이 되었다.
         // computer가 power를 implements를 했기 때문에 가능하다.
         // power를 implements한 객체면 어느 객체가 와도 사용 가능하다.
-        CentralControl centralControl2 = new CentralControl(computer1, led, speaker);
         // 다른 객체지만 연결할 수 있게 해주는 것을 interface의 역할이라고 한다.
+
+        centralControl1.addDevice(computer1);
+        centralControl1.addDevice(speaker);
+        centralControl1.addDevice(led);
+
+
         centralControl1.powerOn();
         System.out.println("------------------");
         centralControl1.powerOff();
-        System.out.println("------------------");
-        centralControl2.powerOn();
-        System.out.println("------------------");
-        centralControl2.powerOff();
+
+
+
+
     }
 }
