e79376d4e4825dc68483c4e001ec77364fda7fab
dozero25 <ehdud05264@naver.com>
2022-11-12 20:20:27 +0900

    11/12 Java 백준 실습 아스키코드 출력

---
+++ Java Intellij 실습/JavaStudy_20221018/src/BackJun/문자열/AsCode.java
@@ -0,0 +1,16 @@
+package BackJun.문자열;
+
+import java.util.Scanner;
+
+public class AsCode {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int c = scanner.next().charAt(0);
+
+        System.out.println(c);
+
+
+    }
+}
