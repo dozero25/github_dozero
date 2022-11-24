14d2ecd3597c56093ad423c8112394f2b39d7211
dozero25 <ehdud05264@naver.com>
2022-11-25 01:36:48 +0900

    11/25 Java Object 정리

---
+++ 개념정리 파일/정리/Object/Teacher.java
@@ -0,0 +1,35 @@
+package 정리.Object;
+
+import java.util.Objects;
+
+public class Teacher {
+
+    private String name;
+    private String schoolName;
+
+    public Teacher(String name, String schoolName) {
+        this.name = name;
+        this.schoolName = schoolName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return Objects.equals(name, teacher.name) && Objects.equals(schoolName, teacher.schoolName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, schoolName);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", schoolName='" + schoolName + '\'' +
+                '}';
+    }
+}
