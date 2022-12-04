620685d801c3e25515527c239e58e36bc0e8cd9f
dozero25 <ehdud05264@naver.com>
2022-12-04 23:48:38 +0900

    12/4 수정 및 로그인 페이지 / encodingFilter / tset1 실습

---
+++ spring(eclipse)개인실습/Servlet-ydy/src/main/java/servlet/account/Login.java
@@ -0,0 +1,20 @@
+package servlet.account;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/account/login.html").forward(request, response);
+	}
+
+
+}
