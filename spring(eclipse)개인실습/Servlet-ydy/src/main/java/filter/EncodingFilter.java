8bf155e8e2d1a76157ae0694dca94f13292f9566
dozero25 <ehdud05264@naver.com>
2022-12-04 23:49:16 +0900

    12/4 register 페이지 실습 -> 404 오류 뜸

---
+++ spring(eclipse)개인실습/Servlet-ydy/src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,45 @@
+package filter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+
+@WebFilter("/EncodingFilter")
+public class EncodingFilter extends HttpFilter implements Filter {
+       
+	private static final long serialVersionUID = 1L;
+
+	
+	public void destroy() {}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest hsr = (HttpServletRequest) request; // 다운캐스팅
+
+		System.out.println(hsr.getMethod());
+		
+		if(hsr.getMethod().equalsIgnoreCase("POST")) {
+			request.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		}
+		
+		// 전처리
+		chain.doFilter(request, response);
+		// 후처리
+	}
+
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
---
+++ spring(eclipse)개인실습/Servlet-ydy/src/main/java/service/AccountService.java
@@ -0,0 +1,43 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.User;
+import repository.UserRepository;
+
+public class AccountService {
+	
+private static AccountService instance = null;
+	
+	
+	private AccountService() {}
+	
+	public static AccountService getInstance() {
+		if(instance == null) {
+			instance = new AccountService();
+		}
+		return instance;
+	} 
+	
+	// username 중복 확인
+	public boolean isDuplicateUsername(String username) {
+		User user = UserRepository.getInstance().findUserByUsername(username);
+		
+		return user !=null;
+	}
+	
+	public void register(User user) {
+		UserRepository.getInstance().saveUser(user);
+	}
+	
+	public User loadUserByUsername(String username) {
+		return UserRepository.getInstance().findUserByUsername(username);
+	}
+	
+	public boolean checkPassword(User user, String password){
+		return user.getPassword().equals(password);
+	}
+	
+
+}
